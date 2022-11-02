732752c79d7c850efd42a1ff9d197f8835dbbae0
juk0327@naver.com <juk0327@naver.com>
2022-11-02 12:34:03 +0900

    css update

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './VideoList.module.css';
 import VideoItem from '../video_item/VideoItem';
 
     const VideoList = (props) => {
         return (
-            <ul>
+            <ul className={styles.videos}>
                 {props.videos.map(video => <VideoItem key={video.id} video={video}></VideoItem>)}
             </ul>
         )
--- src/components/video_item/VideoItem.jsx
+++ src/components/video_item/VideoItem.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import styles from './VideoItem.module.css';
 
- const VideoItem = props => <h1>{props.video.snippet.title}</h1>
+//const VideoItem = props => (~~ img src={props.video.~~}) 대신 밑에처럼 사용가능
+ const VideoItem = ({video: { snippet }}) => (
+    <li className={styles.video}>
+        <img 
+          className={styles.thunbnail}
+          src={snippet.thumbnails.medium.url} 
+          alt="video thumbnail" 
+        />
+        <div>
+            <h1 className={styles.title}>{snippet.title}</h1>
+            <p className={styles.channel}>{snippet.channelTitle}</p>
+            <p className={styles.publishedAt}>{snippet.publishedAt}</p>
+        </div>
+    </li>
+ )
 
 export default VideoItem;
 
